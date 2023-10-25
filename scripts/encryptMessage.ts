09ba3351c24f94bb0e3acfd96e0aece2d4e5dd52
shaxraj.2021 <shaxraj.2021@stud.nstu.ru>
2023-10-25 10:58:16 +0700

    fix: incorrect essay encryption

--- scripts/encryptMessage.ts
+++ scripts/encryptMessage.ts
@@ -5,17 +5,6 @@ if (key.length !== abc.length) {
   console.error('Длина ключа шифрования в message.json не совпадает с длиной алфавита')
 }
 
-const eTable = new Array(abc.length).fill(0)
-
-for (let i = 0; i < key.length; i++) {
-  const k = abc.findIndex((c) => c === key[i])
-  if (k === -1) {
-    console.error(`Алфавит не содержит символ '${key[i]} (${i})'`)
-  }
-
-  eTable[k] = i
-}
-
 const fMsg = message.toLocaleLowerCase()
 
 let eMsg = ''
@@ -23,8 +12,7 @@ for (let i = 0; i < fMsg.length; i++) {
   const cI = abc.findIndex((c) => c === fMsg[i])
   if (cI === -1) continue
 
-  eMsg += abc[eTable[cI]]
+  eMsg += key[cI]
 }
 
-console.log('Зашифрованное сообщение:')
-console.log(eMsg)
+console.log('Зашифрованное сообщение:', '\n' + eMsg)
